92e85603e9c110b0a1085f762d6c4ad7b701cb92
Anees040 <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-04-26 23:28:21 +0500

    Add src/models/SuiteRoom.java

---
+++ src/models/SuiteRoom.java
@@ -0,0 +1,35 @@
+package models;
+
+/**
+ * Concrete room model for suite occupancy.
+ */
+public class SuiteRoom extends AbstractRoom {
+
+    /**
+     * Constructs a suite room.
+     *
+     * @param roomNumber room number
+     * @param floor floor number
+     * @param pricePerMonth monthly price
+     * @param isAvailable availability
+     * @param amenities amenities list
+     */
+    public SuiteRoom(String roomNumber, int floor, double pricePerMonth, boolean isAvailable, String amenities) {
+        super(roomNumber, floor, pricePerMonth, isAvailable, amenities);
+    }
+
+    @Override
+    public String getRoomType() {
+        return "Suite";
+    }
+
+    @Override
+    public double getPrice() {
+        return getPricePerMonth();
+    }
+
+    @Override
+    public int getCapacity() {
+        return 4;
+    }
+}
