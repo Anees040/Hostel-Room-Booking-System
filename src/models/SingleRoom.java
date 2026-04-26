33b7d7414d58eefcf1e2ab45190ecf9176a9077e
Anees040 <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-04-26 23:28:20 +0500

    Add src/models/SingleRoom.java

---
+++ src/models/SingleRoom.java
@@ -0,0 +1,35 @@
+package models;
+
+/**
+ * Concrete room model for single occupancy.
+ */
+public class SingleRoom extends AbstractRoom {
+
+    /**
+     * Constructs a single room.
+     *
+     * @param roomNumber room number
+     * @param floor floor number
+     * @param pricePerMonth monthly price
+     * @param isAvailable availability
+     * @param amenities amenities list
+     */
+    public SingleRoom(String roomNumber, int floor, double pricePerMonth, boolean isAvailable, String amenities) {
+        super(roomNumber, floor, pricePerMonth, isAvailable, amenities);
+    }
+
+    @Override
+    public String getRoomType() {
+        return "Single";
+    }
+
+    @Override
+    public double getPrice() {
+        return getPricePerMonth();
+    }
+
+    @Override
+    public int getCapacity() {
+        return 1;
+    }
+}
