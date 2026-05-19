7072a26d6a2bd0e07047c9720a8baa90310f7cd9
Anees <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-05-19 22:01:20 +0500

    Refactor and refine Saveable.java for Hostel Room Booking System

--- src/interfaces/Saveable.java
+++ src/interfaces/Saveable.java
@@ -1,7 +1,8 @@
 package interfaces;
 
 /**
- * Defines persistence operations for manager classes.
+ * Defines persistence operations for manager classes (Lab 12).
+ * Provides default getDataDirectory() method.
  */
 public interface Saveable {
 
@@ -14,4 +15,27 @@ public interface Saveable {
      * Loads data from storage into memory.
      */
     void load();
+
+    /**
+     * Convenience alias for save(). Calls save() by default.
+     */
+    default void saveData() {
+        save();
+    }
+
+    /**
+     * Convenience alias for load(). Calls load() by default.
+     */
+    default void loadData() {
+        load();
+    }
+
+    /**
+     * Returns the default data directory path.
+     *
+     * @return data directory path
+     */
+    default String getDataDirectory() {
+        return "data/";
+    }
 }
