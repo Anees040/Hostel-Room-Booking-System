010612f5770887b2b583cc19cfdedfabae696bea
Anees <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-05-20 11:14:06 +0500

    Update LoginFrame.java

--- src/gui/LoginFrame.java
+++ src/gui/LoginFrame.java
@@ -16,7 +16,6 @@ import java.awt.Insets;
 import java.awt.RenderingHints;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
